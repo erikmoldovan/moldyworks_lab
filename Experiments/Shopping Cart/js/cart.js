3e251312b5dc71a62b898564fd7fcd46fb4aab4f
Erik Moldovan <erik.moldovan@door3.com>
2012-09-20 16:08:45 -0400

    More mods to the shopping cart. Added pictures, a search function and started on some jQuery

--- Experiments/Shopping Cart/js/cart.js
+++ Experiments/Shopping Cart/js/cart.js
@@ -1,43 +1,119 @@
 // Variable and Object Declaration
-var items = [];
+var items = {catalog:[],
 
-var item = function(obj){$.extend(this, obj)
+    displayItems:function () {
+        var output = document.getElementById('items');
+        var code = '';
+        var currentItem;
+
+        output.innerHTML = output.innerHTML + code;
+
+        for (i = 0; i < this.catalog.length; i++) {
+            currentItem = this.catalog[i];
+            code = this.generateItem(currentItem);
+            output.innerHTML = output.innerHTML + code;
+        }
+
+        var buyBtns = document.getElementsByClassName('buy');
+        for(x = 0; x < buyBtns.length; x++){buyBtns[x].onclick = buyItem;;}
+
+        //$('.buy').click(buyItem);
+    },
+
+    generateItem:function(currentItem){
+        code = '';
+        code += '<div id="' + currentItem.id + '" class="item">';
+        code += '<div class="name">' + currentItem.name + '</div>';
+        code += '<div class="price">$' + currentItem.price.toFixed(2) + '</div>';
+        code += '<div class="author">' + currentItem.author + '</div>';
+        code += '<div class="description">' + currentItem.description + '</div>';
+        code += '<div class="image"><img src="' + currentItem.image + '"/></div>';
+        code += '<input class="buy" id="button-' + currentItem.id + '" type="button" value="Buy"/>';
+        code += '</div>';
+
+        return code;
+    },
+
+    searchFor:function (e) {
+        // Takes input from text field, on button click
+        var input = document.getElementById('searchField').value.toLowerCase();
+        input = input.split(" ");
+        var output = '';
+
+        // Loop through items array
+        for (i = 0; i < items.catalog.length; i++) {
+            // In loop, select items that match input by name. Set their display value to true
+            var allMatch = true;
+
+            for(c = 0; c < input.length; c++){
+                if(items.catalog[i].name.toLowerCase().indexOf(input[c]) == -1){
+                    allMatch = false;
+                    break;
+                }
+            }
+
+            if(allMatch){output += items.generateItem(items.catalog[i]);}
+        }
+
+        document.getElementById('searchResults').innerHTML = output;
+    }
+};
+
+var item = function (obj) {
+    $.extend(this, obj)
     this.name = obj.name || '',
         this.price = obj.price || 0,
-        this.id = obj.id || items.length,
+        this.id = obj.id || items.catalog.length,
+        this.author = obj.author || '',
         this.description = obj.description || '',
         this.image = obj.image || '',
-        this.quantity = obj.quantity || 0};
+        this.quantity = obj.quantity || 0,
+        this.display = obj.display || false;
+} // Does not display items by default
 
-items.push(new item({
-    name: 'Book1',
-    price: 19,
-    description: 'Nada',
-    quantity: 2}));
+items.catalog.push(new item({
+    name:'The Third Wheel',
+    price:7.75,
+    author:'Jeff Kinney',
+    description:'Love is in the air—but what does that mean for Greg Heffley?',
+    image:'images/thirdwheel.jpg',
+    quantity:2}));
 
-items.push(new item({
-    name: 'Book2',
-    price: 23,
-    description: 'Blah',
-    quantity: 3}));
+items.catalog.push(new item({
+    name:'The Racketeer',
+    price:17.06,
+    author:'John Grisham',
+    description:'Who is the Racketeer? And what does he have to do with the judge’s untimely demise? His name, for the moment, is Malcolm Bannister. Job status? Former attorney. Current residence? The Federal Prison Camp near Frostburg, Maryland.',
+    image:'images/racketeer.jpg',
+    quantity:3}));
 
-items.push(new item({
-    name: 'Book3',
-    price: 29,
-    description: 'College',
-    quantity: 4}));
+items.catalog.push(new item({
+    name:'Mad River',
+    price:16.60,
+    author:'John Sandford',
+    description:'Bonnie and Clyde, they thought. And what’s-his-name, the sidekick. Three teenagers with dead-end lives, and chips on their shoulders, and guns.',
+    image:'images/madriver.jpg',
+    quantity:4}));
+
+items.catalog.push(new item({
+    name:'The Mark of Athena',
+    price:11.04,
+    author:'Rick Riordan',
+    description:'In The Son of Neptune, Percy, Hazel, and Frank met in Camp Jupiter, the Roman equivalent of Camp Halfblood, and traveled to the land beyond the gods to complete a dangerous quest. The third book in the Heroes of Olympus series will unite them with Jason, Piper, and Leo. But they number only six--who will complete the Prophecy of Seven?',
+    image:'images/athena.jpg',
+    quantity:4}));
 
 // The cart object/function... method? Controls logic for adding items to the cart, removing them, and calculating the total price.
 var cart = {
-    contents: [],
+    contents:[],
 
-    addItem: function(newItem, quantity){
+    addItem:function (newItem, quantity) {
         var code = '';
         var that = this;
         var price, name;
 
-        for(i = 0; i < quantity; i++){
-            if(newItem.quantity <= 0){
+        for (i = 0; i < quantity; i++) {
+            if (newItem.quantity <= 0) {
                 break;
             }
 
@@ -57,16 +133,16 @@ var cart = {
         $('.remove').unbind().click(deleteItem);
     },
 
-    removeItem: function(oldItemID){
-        for(i= 0; i < this.contents.length; i++){
-            if($('li.cart-' + oldItemID + ':eq(0)')){
+    removeItem:function (oldItemID) {
+        for (i = 0; i < this.contents.length; i++) {
+            if ($('li.cart-' + oldItemID + ':eq(0)')) {
                 $('li.cart-' + oldItemID + ':eq(0)').remove();
                 this.contents.pop(i);
 
                 // Ups the quantity by one for the removed item in the main items array
-                for(x = 0; x < items.length; x++){
-                    if(parseInt(items[x].id) == oldItemID){
-                        items[x].quantity += 1;
+                for (x = 0; x < items.catalog.length; x++) {
+                    if (parseInt(items.catalog[x].id) == oldItemID) {
+                        items.catalog[x].quantity += 1;
                         break;
                     }
                 }
@@ -77,11 +153,11 @@ var cart = {
         }
     },
 
-    calculateTotal: function(){
+    calculateTotal:function () {
         var total = 0;
         var output = document.getElementById('total');
 
-        for(i = 0; i < this.contents.length; i++){
+        for (i = 0; i < this.contents.length; i++) {
             total += parseInt(this.contents[i].price);
         }
 
@@ -90,33 +166,21 @@ var cart = {
 };
 
 // The initialization method. Runs on window load, thanks to statement at the bottom of the script
-function init(){
+function init() {
     cart.calculateTotal();
-
-    var output = document.getElementById('items');
-    var code = '';
-
-    for(i = 0; i < items.length; i++){
-        code = '';
-        code += '<div id="' + items[i].id + '" class="item">';
-        code += '<div class="name">' + items[i].name + '</div>';
-        code += '<div class="price">$' + items[i].price.toFixed(2) + '</div>';
-        code += '<div class="description">' + items[i].description + '</div>';
-        code += '<div class="image"><img src="' + items[i].image + '"/></div>';
-        code += '<input class="buy" id="button-' + items[i].id + '" type="button" value="Buy"/>';
-        code += '</div>';
-        output.innerHTML = output.innerHTML + code;
-    }
-
-    $('.buy').click(buyItem);
+    items.displayItems();
+    document.getElementById('searchBtn').onclick = items.searchFor;
 }
 
-var buyItem = function(e){
-    cart.addItem(items[parseInt(e.target.id.substr(7))], 1);
+var buyItem = function (e) {
+    console.log(e.target.id.substr(7));
+    cart.addItem(items.catalog[parseInt(e.target.id.substr(7))], 1);
 }
 
-var deleteItem = function(e){
+var deleteItem = function (e) {
     cart.removeItem(parseInt(e.target.id.substr(9)), 1);
 }
 
-window.onload = init;
+//window.onload = init;
+$(document).ready(function(){init();
+console.log($('body'),$(document.getElementsByTagName('body')[0]));});
