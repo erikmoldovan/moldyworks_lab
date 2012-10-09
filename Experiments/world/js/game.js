9033944c6e63359f311a35b4dc5a852ed3eef392
Erik Moldovan <erik.moldovan@door3.com>
2012-10-09 17:57:13 -0400

    - Worked a bit on fill/stroke. Needs a lot more work. Probably need to use arcs instead of circles.

--- Experiments/world/js/game.js
+++ Experiments/world/js/game.js
@@ -9,29 +9,52 @@ function drawWorld(){
     var c=document.getElementById("myCanvas");
     var ctx=c.getContext("2d");
     ctx.beginPath();
-    ctx.arc(300,300,120,0,2*Math.PI);
+    ctx.arc(300,300,120,0.5*Math.PI,1.5*Math.PI);
+    ctx.fill();
+    ctx.arc(300,300,120,1.5*Math.PI,0.5*Math.PI);
+    ctx.stroke();
+
+    var timestampOne, timestampTwo;
+
+    ctx.beginPath();
+    ctx.moveTo(285,130);
+    ctx.lineTo(315,150);
+    ctx.moveTo(285,170);
+    ctx.lineTo(315,150);
     ctx.stroke();
 
     $(document).keypress(function(e){
 
         var keyPressed = e.charCode || e.keyCode;
-        console.log(keyPressed);
 
         if(keyPressed == 119){
-            ctx.clearRect(285, 130, 30, 40);
+            ctx.clearRect(0,0,600,600);
             ctx.beginPath();
             ctx.moveTo(285,150);
             ctx.lineTo(315,130);
             ctx.moveTo(285,150);
             ctx.lineTo(315,170);
             ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(300,300,120,0.5*Math.PI,1.5*Math.PI);
+            ctx.stroke();
+            ctx.arc(300,300,120,1.5*Math.PI,0.5*Math.PI);
+            ctx.fill();
+
         }else if(keyPressed == 101){
-            ctx.clearRect(285, 130, 30, 40);
+            ctx.clearRect(0,0,600,600);
             ctx.beginPath();
             ctx.moveTo(285,130);
             ctx.lineTo(315,150);
             ctx.moveTo(285,170);
             ctx.lineTo(315,150);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(300,300,120,0.5*Math.PI,1.5*Math.PI);
+            ctx.fill();
+            ctx.arc(300,300,120,1.5*Math.PI,0.5*Math.PI);
             ctx.stroke();
         }
     });
