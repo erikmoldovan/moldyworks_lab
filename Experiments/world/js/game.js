391404257bd15035303a1be2d108b3165cac2573
Erik Moldovan <erik.moldovan@door3.com>
2012-10-10 19:04:32 -0400

    - MultiDiv experiment for StackOverflow - Trying to make the wheel spin continuously

--- Experiments/world/js/game.js
+++ Experiments/world/js/game.js
@@ -5,16 +5,30 @@
  * Time: 4:02 PM
  * To change this template use File | Settings | File Templates.
  */
+
 function drawWorld(){
     var c=document.getElementById("myCanvas");
     var ctx=c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(300,300,120,0.5*Math.PI,1.5*Math.PI);
-    ctx.fill();
-    ctx.arc(300,300,120,1.5*Math.PI,0.5*Math.PI);
-    ctx.stroke();
 
-    var timestampOne, timestampTwo;
+    var colors = ["black", "white", "black"];
+    var newDir, lastDir;
+    var flipFlag = false;
+
+    var startAngle = 0;
+    var arc = Math.PI / 6;
+    var outsideRadius = 150;
+    var insideRadius = 90;
+
+    for(i = 0; i < 12; i++){
+        var angle = startAngle + i * arc;
+        ctx.fillStyle = colors[(i%2)+1];
+
+        ctx.beginPath();
+        ctx.arc(300, 340, outsideRadius, angle, angle + arc, false);
+        ctx.arc(300, 340, insideRadius, angle + arc, angle, true);
+        ctx.stroke();
+        ctx.fill();
+    }
 
     ctx.beginPath();
     ctx.moveTo(285,130);
@@ -28,37 +42,68 @@ function drawWorld(){
         var keyPressed = e.charCode || e.keyCode;
 
         if(keyPressed == 119){
-            ctx.clearRect(0,0,600,600);
-            ctx.beginPath();
-            ctx.moveTo(285,150);
-            ctx.lineTo(315,130);
-            ctx.moveTo(285,150);
-            ctx.lineTo(315,170);
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.arc(300,300,120,0.5*Math.PI,1.5*Math.PI);
-            ctx.stroke();
-            ctx.arc(300,300,120,1.5*Math.PI,0.5*Math.PI);
-            ctx.fill();
-
+            newDir = "left";
         }else if(keyPressed == 101){
-            ctx.clearRect(0,0,600,600);
-            ctx.beginPath();
-            ctx.moveTo(285,130);
-            ctx.lineTo(315,150);
-            ctx.moveTo(285,170);
-            ctx.lineTo(315,150);
-            ctx.stroke();
+            newDir = "right";
+        }
+
+        console.log('newDir: ' + newDir + ', lastDir: ' + lastDir);
+
+        if(newDir == lastDir){
+            flipFlag = true;
+        }
+
+        console.log(flipFlag);
+
+        worldFill(flipFlag, newDir);
+    });
+
+    function worldFill(doFlip, newDirection){
+
+        var directionMod;
+        var i;
+
+        if(flipFlag){
+            switch(newDirection){
+                case "right":
+                    newDirection = "left";
+                    break;
+                case "left":
+                    newDirection = "right";
+                    break;
+            }
+        }
+
+        /*if(newDirection == "right"){
+            directionMod = i%2;
+        }else if(newDirection == "left"){
+            directionMod = i%2+1;
+        }
+
+        for(i = 0; i < 12; i++){
+            angle = startAngle + i * arc;
+            ctx.fillStyle = colors[directionMod];
 
             ctx.beginPath();
-            ctx.arc(300,300,120,0.5*Math.PI,1.5*Math.PI);
-            ctx.fill();
-            ctx.arc(300,300,120,1.5*Math.PI,0.5*Math.PI);
+            ctx.arc(300, 340, outsideRadius, angle, angle + arc, false);
+            ctx.arc(300, 340, insideRadius, angle + arc, angle, true);
             ctx.stroke();
+            ctx.fill();
         }
-    });
+
+        ctx.clearRect(0,0,600,600);
+        ctx.beginPath();
+        ctx.moveTo(285,130);
+        ctx.lineTo(315,150);
+        ctx.moveTo(285,170);
+        ctx.lineTo(315,150);
+        ctx.stroke();*/
+
+        lastDir = newDirection;
+    }
 }
+
+
 
 function init(){
     drawWorld();
