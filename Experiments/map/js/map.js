8ceb040feadc8e5e034ae3ae2a5b5c01a9aeecd4
Erik Moldovan <erik.moldovan@door3.com>
2012-12-20 18:23:39 -0500

    Lots of rework to make boxes dynamic

--- Experiments/map/js/map.js
+++ Experiments/map/js/map.js
@@ -18,30 +18,99 @@ $(document).ready(function() {
     });
 
     var d3Map = {
+        
+        boxesLeft:830,
+        boxesTop:200,
+        boxesWidth:48,
+        boxesHeight:25,
+        boxesXPadding:2,
+        boxesYPadding:2,
+                
+        stateBoxIDs:  ["09", "10", "25", "24", "33", "34", "44", "50"],
+        sBoxes: [],
+        
         init: function() {
 
-            var thisMap = this;
+            var position, currentState;
+            var map = this;
 
             var drawMap = d3.select('#mapContainer').append('svg')
                 .attr("width", 960)
                 .attr("height", 500);
 
              d3.json("data/states_final.json", function(collection) {
+                $.each(collection.features, function(){
+                    currentState = this;
+                    position = $.inArray(currentState.id, map.stateBoxIDs);
+                    if(position !== -1){
+                        map.getSBoxesValues(currentState, parseInt(position));
+                    }
+                })
+                var z ={}; z.code = '3';
+                 
                 drawMap.selectAll("path")
                     .data(collection.features)
                     .enter().append("path")
                     .attr("d", d3.geo.path().projection(d3.geo.albersUsa()))
-                    .style("fill", thisMap.fillStates)
-                    .on("click", thisMap.stateClick)
-                    .on("mouseover", thisMap.hoverOnState)
-                    .on("mouseout", thisMap.hoverOutState);
+                    .style("fill", map.fillStates)
+                    .on("click", map.stateClick)
+                    .on("mouseover", map.hoverOnState)
+                    .on("mouseout", map.hoverOutState);
+                    
+                map.drawSBoxes(drawMap);
             });
-
         },
+
+        getSBoxesValues: function(originalState, posID){
+            var stateID = originalState.id,
+                abbrev = originalState.properties.abbrev,
+                code = originalState.code,
+                url = originalState.url;
+            
+            var currentBox = {};
+            var isEven = posID % 2 == 0;
+            
+            if(isEven){
+                currentBox.x = this.boxesLeft;
+                currentBox.y = this.boxesTop + ((this.boxesHeight + this.boxesYPadding)*(posID/2))
+            }
+            if (!isEven){
+                currentBox.x = this.boxesLeft + this.boxesWidth + this.boxesXPadding; 
+                currentBox.y = this.boxesTop + ((this.boxesHeight + this.boxesYPadding)*((posID-1)/2))
+            }
+            currentBox.code = code;
+            currentBox.url = url;
+            
+            this.sBoxes.push(currentBox);
+        },
+        
+        drawSBoxes: function(drawMap){
+            var that = this,
+            stateboxes = this.sBoxes;
+            drawMap.selectAll("rect")
+                .data(stateboxes)
+                .enter().append("rect")
+                .attr("x", function(index){return index.x})
+                .attr("y", function(index){return index.y})
+                .attr("height", that.boxesHeight)
+                .attr("width", that.boxesWidth)
+                .style("fill", that.fillStates)
+                .on("click", that.stateClick)
+                .on("mouseover", that.hoverOnState)
+                .on("mouseout", that.hoverOutState);
+                
+//            drawMap.append('text')
+//                .style("color", "black")
+//                .attr("transform", "translate(" + 100 + ",100)")
+//                .text("Bob");
+        },
+        
         fillStates: function(d){
             if(d.code == 3) return "blue";
             else if(d.code == 2) return "red";
             else if(d.code == 1) return "green";
+            
+            return false;
         },
         hoverOnState: function(d){
             d3.select(this).style("fill", "yellow");
