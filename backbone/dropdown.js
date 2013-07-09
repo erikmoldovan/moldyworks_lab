cbd6f99b9bd990bd8ce81096e8b3d7021dc859d3
moldy <moldy@devtastic.(none)>
2013-07-09 17:48:25 -0400

    Working on implementing proper event handling using Backbone's builtin handlers. Which, incidentally, also forces me to use the models for data storage rather than relying on the DOM.

--- backbone/dropdown.js
+++ backbone/dropdown.js
@@ -5,31 +5,89 @@
  * Time: 11:15 PM
  * To change this template use File | Settings | File Templates.
  */
-
 $(function(){
-  //$(".chzn-select").chosen();
+  /* JSON Parser function */
+  function parseObj(link){
+    var ajaxResponse = {};
+    $.ajax({
+      url: link,
+      async: false,
+      success: function(data){
+        ajaxResponse = data;
+      }
+    });
+    return ajaxResponse.dropdown;
+  }
+  // Call it!
+  var feed_obj = parseObj('dropdown.json');
 
-  // Model
+  /* Model for Header + Options list */
   var optModel = Backbone.Model.extend({
-    // Sets default structure and data for model
     defaults: {
-      optionText: 'Option',
-      headerText: 'Header',
-      color: '#000000'
+      option_text: 'Option',
+      header_text: 'Header',
+      color: '#000000',
+      current: false
     }
   })
 
-  // Collection
+  /* Collection template for optModel */
   var optCollection = Backbone.Collection.extend({
-    model: optModel, // Defines model in use
-    url: 'dropdown.json', // URL of collection. In this case, JSON file
-    // Called when Collection makes fetch call. This parses returns the JSON object
-    parse: function(response){
-      return response.dropdown;
+    model: optModel
+  })
+  // Instantiate the collection using the Header+Options Model
+  var optList = new optCollection([
+    $.each(feed_obj, function(key, value){
+      new optModel({option_text: value.optionText, header_text: value.headerText, color: value.color});
+    })
+  ]);
+
+  /* View to handle HTML rendering */
+  var optView = Backbone.View.extend({
+    tagName: 'li',
+
+    initialize: function(){
+      this.listenTo(this.model, 'change', this.render);
+      this.render;
+    },
+
+    render: function(){
+      this.$el.html('<div name=' + this.model.get('header_text') + ' color=' + this.model.get('color') + '>' + this.model.get('option_text') + '</div>');
+      return this;
     }
   })
 
-  // View
+  /* View for the App */
+  var App = Backbone.View.extend({
+    el: $('.selectContainer'),
+
+    initialize: function(){
+      // this.header = $('#headerText');
+      // this.current = $('#currentOption');
+      this.list = $('#selectList');
+
+      // this.listenTo(optList, 'change', this.render);
+
+      console.log(optList);
+
+      optList.each(function(option){
+        var view = new optView({ model: option});
+        this.list.append(view.render().el);
+      }, this);
+    },
+
+    render: function(){
+      // var current = {};
+
+      // _.each(optList.getCurrent(), function(elem){
+      //   current.header_text = elem.get('header_text');
+      //   current.option_text = elem.get('option_text');
+      //   current.color = elem.get('color');
+      // })
+    }
+  })
+
+/*  // View
   var optView = Backbone.View.extend({
     // Scope of the view; the base element
     el: $('#selectList'),
@@ -49,16 +107,14 @@ $(function(){
 
       return this;
     }
-  });
+  });*/
 
   // This is the last I could do. I didn't grasp the events system in time to implement this functionality properly
-    $('#selectList').click(function(element){
-      console.log(element.target.textContent);
-      console.log($(element.target));
-      $('#topItem').text(element.target.textContent);
-      $('#headerText span').text(element.target.attributes[0].nodeValue);
-      $('#headerText').css("background-color", element.target.attributes[1].nodeValue);
-  });
+/*  $('#selectList').click(function(element){
+    $('#topItem').text(element.target.textContent);
+    $('#headerText span').text(element.target.attributes[0].nodeValue);
+    $('#headerText').css("background-color", element.target.attributes[1].nodeValue);
+  });*/
 
-  new optView();
+  new App();
 });
