2d32a63653f56129f14877792f4c5a738ca931eb
Erik Moldovan <erikmoldovan@gmail.com>
2013-08-30 12:59:22 -0400

    Merging d3map and dg_parser drupal projects into the Labs repo

---
+++ Drupal/d3map/d3map.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file d3map.tpl.php
+ */
+?>
+<div id="pBody">
+    <div id="stateInfo">
+        <div class="stateName">
+        </div>
+        <div class="statePolicy">
+        </div>
+    </div>
+    <div id="mapContainer">
+    </div>
+</div>
